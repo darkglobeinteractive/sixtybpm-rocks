3a9f6e8bfb5a318c59e08490c5e888d866299fc2
Robbie Schneider <zerodegreesk@hotmail.com>
2022-02-27 20:42:57 -0500

    Updating Title on Main Page
    I coped and pasted the DGD site.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Dark Globe Development</h1>
+        <h1>60bpm Rocks!</h1>
         <p>Coming Soon</p>
       </div>
     );
