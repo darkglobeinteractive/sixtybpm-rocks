455b1ae1ce0e36daa6b545328fe56b98ca587cd6
Robbie Schneider <zerodegreesk@hotmail.com>
2022-02-27 20:03:54 -0500

    Added boilerplate and fixed the css-mini-extract-plugin issue.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dark Globe Development</h1>
+        <p>Coming Soon</p>
+      </div>
+    );
+  }
+}
+
+export default App;
