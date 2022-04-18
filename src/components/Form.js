47d68c97f36ee37e69e35e34d0f7d69a6a5e53e0
Robbie Schneider <darkglobeinteractive@gmail.com>
2022-04-18 18:53:07 -0400

    Forcing Uppercase Letters In Code
    Adding a bit of code to force uppercase letters in the code entry field.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../css/Form.css';
 
 const Form = () => {
+
+  // Create local state to uppercase letters
+  const [code, setCode] = useState('');
+
   return (
     <div id="form-container">
       <div className="wrap">
         <form id="form" action="https://60bpm.com/roaming-stones/" method="post">
           <div className="field-set">
             <label htmlFor="rocks-check">Enter the code found on the back of your stone below and click submit.</label>
-            <input type="text" id="rocks-check" name="rocks_check" placeholder="Enter Code Here" />
+            <input type="text" id="rocks-check" name="rocks_check" value={code} placeholder="Enter Code Here" onChange={(e) => setCode(e.target.value.toUpperCase())} />
           </div>
           <div className="field-set">
             <input type="submit" value="Submit" />
